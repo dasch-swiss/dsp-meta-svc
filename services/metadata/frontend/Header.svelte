3e9101edd0696b7c0a8f7acc6a3fdc4c1971c733
Balduin Landolt <33053745+BalduinLandolt@users.noreply.github.com>
2022-08-17 11:37:18 +0200

    fix: falsly displayed filter button on large screens

--- services/metadata/frontend/Header.svelte
+++ services/metadata/frontend/Header.svelte
@@ -65,7 +65,7 @@
         </svg>
       </button>
       <!-- filter button -->
-      <button class="filter-button" on:click="{toggleFilters}">
+      <button class="filter-button m-hidden"  on:click="{toggleFilters}">
         <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
         </svg>

f0965db966a9a92c27115e367fa4829ed330e233
Daniela Subotic <daniela.subotic@dasch.swiss>
2022-08-17 17:10:18 +0200

    fixing

--- services/metadata/frontend/project-page/ProjectWidget.svelte
+++ services/metadata/frontend/project-page/ProjectWidget.svelte
@@ -46,12 +46,12 @@
   <div class=label>
     <span style="display:inline">
       Permalink
-    </span>
-      <a class="data" href={getARK()} target=_>Project Permalink</a>
         <button on:click={copyToClipboard} title="copy permalink to the clipboard">
           <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path></svg>
         </button>
+    </span>
   </div>
+  <a class="data" href={getARK()} target=_>{getARK()}</a>
 
   <!-- Shortcode -->
   {#if $projectMetadata?.project.shortcode}
